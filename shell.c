c4f3686a1b098797755d59810f85937c066975e2
jsgalvish <jsgalvish@unal.edu.co>
2019-08-16 13:37:58 -0500

    first version prompt

---
+++ shell.c
@@ -0,0 +1,6 @@
+#include "shell.h"
+
+int main(int argc, char **argv)
+{
+	_get_input(1);
+}
---
+++ shell.h
@@ -0,0 +1,11 @@
+#ifndef _shell_
+#define _shell_
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+void _get_input(int input);
+
+#endif
